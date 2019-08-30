e65fdea5a3557b27fd3dab132c5f4a5967fc4893
Wakeup <17njtan@stu.edu.cn>
2019-08-30 14:39:41 +0800

    Add files via upload
    relation

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import relation from '@/components/relationGraph'
+Vue.use(Router)
 
-Vue.use(Router);
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'relationGraph',
+      component: relation
+    }
 
+  ]
+})
